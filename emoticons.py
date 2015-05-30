9582be364dc9175008a0eec4ab28cfb57df98004
Vincent Castellano <surye80@gmail.com>
2015-05-29 17:00:51 -0700

    Initial Commit

---
+++ emoticons.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+import plugintypes
+
+
+class EmoticonsPlugin(plugintypes.TelegramPlugin):
+    patterns = {
+        "^!lod": "lod"
+        "^!lolidk": "lolidk"
+    }
+
+    usage = [
+        "!lod, !lolidk",
+    ]
+
+    def lod(self, msg, matches):
+        return "ಠ_ಠ"
+
+    def lolidk(self, msg, matches):
+        return "¯\_(ツ)_/¯"
